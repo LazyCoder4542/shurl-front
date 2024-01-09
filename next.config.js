34d0d2c8f347a0ca5b31cbeea941132c1ac3c50f
Adeola Fadare <adeolafadare8@gmail.com>
2024-01-09 06:22:46 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/ap1/v1/:path*',
+        destination:
+          process.env.NODE_ENV === 'production'
+            ? '/ap1/v1/:path*'
+            : 'https://qlnkr.vercel.app/api/v1/:path*',
+      },
+    ];
+  },
+
+}
 
 module.exports = nextConfig
