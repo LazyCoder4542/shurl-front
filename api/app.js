4a7e1c284c1475ac4ce4022613c27b39dd807728
Adeola Fadare <adeolafadare8@gmail.com>
2024-01-09 05:21:05 +0100

    test

--- api/app.js
+++ api/app.js
@@ -1,3 +1,16 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const path = require("path")
+dotenv.config({path: path.join(__dirname, '/../config.env')})
+
+const CS = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(CS).then(c => {
+    console.log('DB connection successful')
+})
+
+////////////////////////////
+
+
 const bodyParser = require("body-parser")
 const app = require("express")()
 
