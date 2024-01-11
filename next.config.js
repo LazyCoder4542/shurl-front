9062e81416c0f058bcce453ca05190a87a23ad69
Adeola Fadare <adeolafadare8@gmail.com>
2024-01-11 11:20:03 +0100

    header

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/ap1/v1/:path*',
-        destination:
-          process.env.NODE_ENV === 'production'
-            ? '/ap1/v1/:path*'
-            : 'https://qlnkr.vercel.app/api/v1/:path*',
-      },
-    ];
-  },
-
-}
+const nextConfig = {}
 
 module.exports = nextConfig
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div>icon</div>
+      <button>login</button>
+      <button>SignUp</button>
+    </header>
+  );
+}
+
+export default Header;
